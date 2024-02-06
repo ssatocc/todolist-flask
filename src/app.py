4202a2b1510009ec103d3405973e31555f89a6a1
ssatocc <ssatocc@gmail.com>
2024-02-06 14:51:48 +0000

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,12 +1,33 @@
+import os
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+db = SQLAlchemy()
 
 
-@app.route("/", methods=["GET"])
-def index():
-    return render_template("index.html")
+def create_app(config=None):
+    app = Flask(__name__)
+
+    if config is None:
+        db_url = "sqlite:///" + os.path.join(os.path.dirname(__file__), "db.sqlite3")
+        config = {
+            "SQLALCHEMY_DATABASE_URI": db_url,
+            "SQLALCHEMY_TRACK_MODIFICATIONS": False,
+        }
+    app.config.update(config)
+
+    db.init_app(app)
+
+    from models import Todo  # noqa
+
+    @app.route("/", methods=["GET"])
+    def index():
+        return render_template("index.html")
+
+    return app
 
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True, host="0.0.0.0", port=3000)
