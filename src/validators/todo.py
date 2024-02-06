3079b381d44d8960c083a39eb5970abd0354492a
ssatocc <ssatocc@gmail.com>
2024-02-06 15:48:29 +0000

    Create src/validators/

---
+++ src/validators/todo.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class TodoValidator:
+    name: str
+
+    def __post_init__(self):
+        self.validate_name()
+
+    def validate_name(self):
+        if not self.name:
+            raise ValueError("Todo must be a non-empty string")
+        if len(self.name) > 50:
+            raise ValueError("Todo must not be longer than 50 characters")
