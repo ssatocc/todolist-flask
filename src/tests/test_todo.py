ce3f5f1bb45ca8f5b21aff414a65bc8a5463d4fe
ssatocc <ssatocc@gmail.com>
2024-02-07 04:35:05 +0000

    Update test_todo.py

--- src/tests/test_todo.py
+++ src/tests/test_todo.py
@@ -2,3 +2,34 @@ class TestTodo:
     def test_index(self, client):
         resp = client.get("/")
         assert resp.status_code == 200
+
+    def test_api_todolist_get(self, client):
+        resp = client.get("/api/todolist")
+        assert resp.status_code == 200
+        assert len(resp.json) == 0
+
+    def test_api_todolist_post(self, client):
+        resp = client.post("/api/todolist", json={"name": "Math"})
+        assert resp.status_code == 200
+
+        resp = client.get("/api/todolist")
+        assert resp.status_code == 200
+        assert len(resp.json) == 1
+        assert resp.json[0]["name"] == "Math"
+
+    def test_api_todolist_done_post(self, client):
+        resp = client.post("/api/todolist/done", json={"ids": [1]})
+        assert resp.status_code == 200
+
+        resp = client.get("/api/todolist")
+        assert resp.status_code == 200
+        assert len(resp.json) == 1
+        assert resp.json[0]["completed"]
+
+    def test_api_todolist_delete(self, client):
+        resp = client.delete("/api/todolist", json={"ids": [1]})
+        assert resp.status_code == 200
+
+        resp = client.get("/api/todolist")
+        assert resp.status_code == 200
+        assert len(resp.json) == 0
